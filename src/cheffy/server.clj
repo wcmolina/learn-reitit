b3560260c410de343472cdd25a0cf10ebca74d65
Wilmer <wcarranzam91@gmail.com>
2022-01-03 16:51:44 -0600

    First response from server

--- src/cheffy/server.clj
+++ src/cheffy/server.clj
@@ -1,5 +1,18 @@
 (ns cheffy.server
-  (:gen-class))
+  (:require [reitit.ring :as ring]
+            [ring.adapter.jetty :as jetty]))
+
+(def app
+  (ring/ring-handler
+    (ring/router
+      [["/"
+        {:get {:handler (fn [req] {:status 200
+                                   :body   "Hello World"})}}]])))
+
+(defn start
+  []
+  (jetty/run-jetty app {:port 3000 :join? false})
+  (println "\n Server running on port 3000..."))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -7,4 +20,7 @@
   (println "Hello, World!"))
 
 (comment
+  (start)
+  (app {:request-method :get
+        :uri "/"})
   (-main))
