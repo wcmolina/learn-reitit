c531abccd6fbaaa626b102b7d53d637ccfb645e1
Wilmer <wcarranzam91@gmail.com>
2022-01-04 21:46:11 -0600

    Start, stop and reset server using Integrant

--- src/cheffy/server.clj
+++ src/cheffy/server.clj
@@ -1,26 +1,51 @@
 (ns cheffy.server
   (:require [reitit.ring :as ring]
-            [ring.adapter.jetty :as jetty]))
+            [ring.adapter.jetty :as jetty]
+            [integrant.core :as ig]
+            [environ.core :refer [env]]))
 
-(def app
+(defn app
+  [env]
   (ring/ring-handler
     (ring/router
       [["/"
         {:get {:handler (fn [req] {:status 200
                                    :body   "Hello World"})}}]])))
 
-(defn start
-  []
-  (jetty/run-jetty app {:port 3000 :join? false})
-  (println "\n Server running on port 3000..."))
+(defmethod ig/prep-key :server/jetty
+  [_ config]
+  ;; Overwrite port from config.edn (use Heroku's)
+  (merge config {:port (Integer/parseInt (env :port))}))
+
+(defmethod ig/init-key :server/jetty
+  ;; Unused arg === _
+  ;; keys is used to destructure map
+  [_ {:keys [handler port]}]
+  (println (str "\nServer running on port " port))
+  (jetty/run-jetty handler {:port port :join? false}))
+
+(defmethod ig/init-key :cheffy/app
+  [_ config]
+  (println "\nStarted app")
+  (app config))
+
+(defmethod ig/init-key :db/postgres
+  [_ config]
+  (println "\nConfigured db")
+  (:jdbc-url config))
+
+(defmethod ig/halt-key! :server/jetty
+  [_ jetty]
+  (.stop jetty))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [config-file]
+  ;; slurp is used to read a file basically
+  ;; ig/read-string will parse those # literals defined in the config.edn file
+  (let [config (-> config-file slurp ig/read-string)]
+    (-> config ig/prep ig/init)))
 
 (comment
-  (start)
   (app {:request-method :get
-        :uri "/"})
+        :uri            "/"})
   (-main))
