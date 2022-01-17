417b37591f4b6785d424be8af8912cd19e88ccd6
Wilmer <wcarranzam91@gmail.com>
2022-01-17 09:42:25 -0600

    Add list recipes test

--- test/cheffy/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns cheffy.core-test
-  (:require [clojure.test :refer :all]
-            [cheffy.server :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
---
+++ test/cheffy/recipe_tests.clj
@@ -0,0 +1,19 @@
+(ns cheffy.recipe-tests
+  (:require [clojure.test :refer :all]
+            [cheffy.server :refer :all]
+            [cheffy.test-system :as ts]))
+
+(deftest recipes-test
+  (testing "List recipes"
+    (testing "with auth -- public and drafts"
+      (let [{:keys [status body]} (ts/test-endpoint :get "/v1/recipes" {:auth true})]
+        (is (= 200 status))
+        (is (vector? (:public body)))
+        (is (vector? (:drafts body)))))
+
+    (testing "without auth -- public"
+      (let [{:keys [status body]} (ts/test-endpoint :get "/v1/recipes" {:auth false})]
+        (is (= 200 status))
+        (is (vector? (:public body)))
+        (is (nil? (:drafts body)))))))
+
